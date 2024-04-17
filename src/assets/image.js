c7fd33a4f3992021cb4da9be2545dcb164d795f5
madhu-c10 <pmadhu.c10@gmail.com>
2024-04-17 21:03:34 +0530

    some changes

--- src/assets/image.js
+++ src/assets/image.js
@@ -1,4 +1,7 @@
-export {default as bannerbackimg} from './Group 2067 (1).png'
-export {default as topfeaturesimg} from './ABD5 1.png';
-export {default as topintegrationimg} from './Component 363.png';
+/* export {default as bannerbackimg} from './Group 2067 (1).png' */
+export {default as topfeaturesimg} from './37 1 (1).svg';
+export {default as topintegrationimg} from './Frame 9725.svg';
 export {default as footerlogo} from './Frame 14.png';
+export {default as bannersubimg1} from './Frame 815.svg';
+export {default as bannersubimg2} from './User Report.svg';
+export {default as frequentlyimg} from './118 2.svg';
--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './banner.css';
 import Button from '../button/Button';
+import { bannersubimg1, bannersubimg2 } from '../../assets/image';
 
 function Banner() {
   return (
@@ -11,21 +12,24 @@ function Banner() {
             <div className='col banner-container'>
             </div>
             <div className='banner-text-container col'>
-            <svg className='banner-svg-one' xmlns="http://www.w3.org/2000/svg" width="81" height="88" viewBox="0 0 81 88" fill="none">
-  <path d="M77.5722 3.67853L69.2896 84.6246L3.32949 36.9786L77.5722 3.67853Z" stroke="url(#paint0_linear_228_14817)" stroke-width="6"/>
-  <defs>
-    <linearGradient id="paint0_linear_228_14817" x1="35.0707" y1="37.147" x2="93.0096" y2="72.7824" gradientUnits="userSpaceOnUse">
-      <stop stop-color="#5CCBF5"/>
-      <stop offset="1" stop-color="#C55BD6"/>
-    </linearGradient>
-  </defs>
-</svg>
+              <svg className='banner-svg-one' xmlns="http://www.w3.org/2000/svg" width="81" height="88" viewBox="0 0 81 88" fill="none">
+                <path d="M77.5722 3.67853L69.2896 84.6246L3.32949 36.9786L77.5722 3.67853Z" stroke="url(#paint0_linear_228_14817)" stroke-width="6" />
+                <defs>
+                  <linearGradient id="paint0_linear_228_14817" x1="35.0707" y1="37.147" x2="93.0096" y2="72.7824" gradientUnits="userSpaceOnUse">
+                    <stop stop-color="#5CCBF5" />
+                    <stop offset="1" stop-color="#C55BD6" />
+                  </linearGradient>
+                </defs>
+              </svg>
               <span className='banner-titte-text'>Active Directory Management </span>
               <span className='banner-second-tittle'>Software at its Best</span>
               <p className='banner-p'>Lorem ipsum dolor sit amet, consectetur adipiscing <br /> elit. Nullam ut sem nec tor</p>
               <button className="button-banner" role="button">Get Started</button>
             </div>
-            <div className='banner-inner-div'></div>
+            <div className='banner-inner-div'>
+            <img src={bannersubimg1} className="img-fluid banner-sub-img" alt="Responsive image" />
+            <img src={bannersubimg2} className="img-fluid banner-sub-img-2" alt="Responsive image" />
+            </div>
             <svg className='banner-bottom-svg' xmlns="http://www.w3.org/2000/svg" width="93" height="93" viewBox="0 0 93 93" fill="none">
               <path d="M6.16979 17.4303L76.0243 6.16927L87.2853 76.0238L17.4308 87.2848L6.16979 17.4303Z" stroke="url(#paint0_linear_228_14816)" stroke-width="10" />
               <defs>
@@ -35,7 +39,7 @@ function Banner() {
                 </linearGradient>
               </defs>
             </svg>
-            
+
           </div>
         </div>
       </section>
